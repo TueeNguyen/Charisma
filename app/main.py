bacf6350aa5b823acccecc292ffc9e42e689b690
davidjtgrant <dave.grant08@gmail.com>
2022-01-15 12:24:37 -0500

    initial commit etherscan API

--- app/main.py
+++ app/main.py
@@ -1,46 +1,36 @@
 import requests
+import json
 from fastapi import FastAPI
 from pydantic import BaseModel
 from starlette.responses import JSONResponse
-import json
 
-from app.config import etherscan_url, etherscan_key, opensea_url, opensea_key
-
+from app.etherscanAPI import getERC721Transactions
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "App is working."}
 
+
 @app.get("/address/{walletAddress}", response_class=JSONResponse)
 async def get_data(walletAddress):
-    
     json_data = {
         "message": "",
         "walletAddress":walletAddress,
         "dimensions":{
-            "D_or_P": getTransactions(walletAddress)
+            "D_or_P": getDorP(walletAddress),
+            "O_or_U": getOorU(walletAddress),
+            "E_or_C": getEorC(walletAddress),
+            "S_or_B": getSorB(walletAddress),
         }
     }
     return JSONResponse(content=json_data)
 
 
-def getTransactions(walletAddress):
+def getDorP(walletAddress):
     walletAddress=walletAddress.lower()
-    url = etherscan_url
-    
-    params = {
-    'module':'account',
-    'action':'tokennfttx',
-    'address':walletAddress,
-    'sort':'asc',
-    'apikey':etherscan_key
-    }
-
-    response = requests.request("GET", url, headers={}, params=params)
-    result = json.loads(response.text)
+    result = getERC721Transactions(walletAddress)
 
     sellCount = 0
     buyCount = 0
@@ -66,3 +56,22 @@ def getTransactions(walletAddress):
     return {"value":value, "description": userMessage}
 
 
+def getOorU(walletAddress):
+    value = 'O'
+    userMessage = f"""Enter text description here"""
+
+    return {"value":value, "description": userMessage}
+
+
+def getEorC(walletAddress):
+    value = 'E'
+    userMessage = f"""Enter text description here"""
+
+    return {"value":value, "description": userMessage}
+
+
+def getSorB(walletAddress):
+    value = 'S'
+    userMessage = f"""Enter text description here"""
+
+    return {"value":value, "description": userMessage}

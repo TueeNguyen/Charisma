26e39587d3535903a89baecc663ee355f6832a9a
davidjtgrant <dave.grant08@gmail.com>
2022-01-15 16:43:01 -0500

    Added handling for No NFT case
    Applicable for getSorB and getDorP
    
    Still need to implement on other functions.

--- app/config.py
+++ app/config.py
@@ -2,10 +2,10 @@ import os
 
 #Etherscan Config
 etherscan_url = 'https://api.etherscan.io/api'
-#etherscan_key = os.environ['etherscan_key']
+etherscan_key = os.environ['etherscan_key']
 #etherscan_key = enter key here if running locally and uncomment line above
 
 #OpenSea Config
 opensea_url = 'https://api.opensea.io/api/v1/'
-#opensea_key = os.environ['opensea_key']
+opensea_key = os.environ['opensea_key']
 #opensea_key = enter key here if running locally and uncomment line above

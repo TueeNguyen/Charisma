4d64af006255aad327cfba6d89096768dd62f817
davidjtgrant <dave.grant08@gmail.com>
2022-01-14 19:59:09 -0500

    added dockerfile

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from starlette.responses import JSONResponse
+
 
 app = FastAPI()
 
@@ -6,3 +9,12 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/address/{walletAddress}", response_class=JSONResponse)
+async def get_data(walletAddress):
+    
+    json_data = {
+        "message": "",
+        "field": walletAddress
+    }
+    return JSONResponse(content=json_data)

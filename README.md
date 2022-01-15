c2a1ac713065d8932dd6f58689e5aa2e540f9f1c
davidjtgrant <dave.grant08@gmail.com>
2022-01-15 10:55:28 -0500

    added config

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 Backend for Wallet Reputation Hackathon PRoject
 
 
-## Instructions to Run
+## Instructions to Run Locally
 1. setup and activate python venv (python version 3.7.3)
 2. install requirements.txt
-
-in terminate at project root, execute: uvicorn app.main:app --reload
-You'll then be able to view: http://127.0.0.1:8000
+3. If running locally, put your api keys into "config.py" and comment out os.environ[] line
+4. At project root, execute: uvicorn app.main:app --reload
+5. You'll then be able to view: http://127.0.0.1:8000
--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
+import requests
 from fastapi import FastAPI
 from pydantic import BaseModel
 from starlette.responses import JSONResponse
+import json
+
+from app.config import etherscan_url, etherscan_key, opensea_url, opensea_key
 
 
 app = FastAPI()
@@ -8,13 +12,57 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "App is working."}
 
 @app.get("/address/{walletAddress}", response_class=JSONResponse)
 async def get_data(walletAddress):
     
     json_data = {
         "message": "",
-        "field": walletAddress
+        "walletAddress":walletAddress,
+        "dimensions":{
+            "D_or_P": getTransactions(walletAddress)
+        }
     }
     return JSONResponse(content=json_data)
+
+
+def getTransactions(walletAddress):
+    walletAddress=walletAddress.lower()
+    url = etherscan_url
+    
+    params = {
+    'module':'account',
+    'action':'tokennfttx',
+    'address':walletAddress,
+    'sort':'asc',
+    'apikey':etherscan_key
+    }
+
+    response = requests.request("GET", url, headers={}, params=params)
+    result = json.loads(response.text)
+
+    sellCount = 0
+    buyCount = 0
+    
+    for transaction in result['result']:
+        print(transaction['from'])
+        if transaction['from']==walletAddress:
+            sellCount+=1
+        if transaction['to']==walletAddress:
+            buyCount+=1
+
+    transactionCount = buyCount+sellCount
+
+    if buyCount > sellCount:
+        value="D"
+    else:
+        value="P"
+
+    userMessage = f"""We analyzed {transactionCount} ERC721 transacations on Etherscan. You had {buyCount} incoming transactions and {sellCount} outgoing transactions."""
+    
+
+    print(buyCount, sellCount)
+    return {"value":value, "description": userMessage}
+
+

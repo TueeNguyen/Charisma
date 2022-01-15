c2b09b1acc5dd0a6b818985621ecbbfaf70f5212
Alex <alexcucos1@gmail.com>
2022-01-14 20:00:51 -0500

    Added web3 library; Implemented metamask connectivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,45 @@
 
 import './App.css';
-// const Web3 = require('web3')
+// import Web3 from 'web3';
+import React, { useState } from 'react'
 
 
-// const web3 = new Web3(Web3.givenProvider) 
+
+
 //const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/c5cd7d6606b14d1b890801407356df7b:8545'))
 
 
-
 function App() {
+  const truncateAccount = (addressList) => {
+    let str = addressList.split("")
+    let ret = "";
+    for(var i = 0; i < 6; i++){
+      ret += str[i]
+    }
+    ret += "..."
+    for(i = addressList.length - 1; i > addressList.length - 5; i--){
+      console.log('str[i]')
+      ret += str[i]
+    }
+    console.log('ret is: ' + ret)
+    return ret
+  }
+
+  let [user, setUser] = useState('Connect your wallet...');
+
+  if(!window.ethereum){
+    alert('No ethereum client detected, try MetaMask!')
+  }else {
+    window.ethereum.request({ method: 'eth_requestAccounts' }).then((addressList) => {
+      setUser(truncateAccount(addressList[0]))
+    })
+  }
+
   return (
     <div className="App">
       <header className='header'>
         <h1>W<span id="blue">R</span></h1>
-        <p>Connect your wallet...</p>
+        <p>{user}</p>
       </header>
       
       <section className="hero">
